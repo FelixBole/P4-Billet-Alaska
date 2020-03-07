a1c943641190bc2092e71cb3b7bb145ca9cebfca
FelixBole <felix.bole@yahoo.fr>
2020-03-07 10:47:07 +0100

    Moved admin nav

--- app/View/admin/chapter/index.php
+++ app/View/admin/chapter/index.php
@@ -1,13 +1,13 @@
 
 
-<h1>Gérer les chapitres</h1>
+<h1>Gestion des chapitres</h1>
 
-<ul class="nav">
-    <li class="nav-item"><a href="?p=admin.comment.index" class="nav-link">Gestion des commentaires</a></li>
-</ul>
 
-<div>
-    <p><a href="?p=admin.chapter.new" class='btn btn-success'>Nouveau chapitre</a></p>
+
+
+<div class="nav mb-2">
+    <a href="?p=admin.chapter.new" class='btn btn-success'>Nouveau chapitre</a>
+    <a href="?p=admin.comment.index" class="nav-link adminNavBtn">Gérer les commentaires</a>
 </div>
 
 <table class='table table-striped table-dark'>
